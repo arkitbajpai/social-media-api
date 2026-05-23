c8baee479acc7c24c96c95beab97cfed4480477b
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2026-05-23 22:25:17 +0530

    just a change

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -43,4 +43,72 @@ try{
 
 
 }}
+export const updatePost=async(req,res)=>{
 
+try{
+
+const post=
+await Post.findById(
+req.params.id
+);
+
+
+if(!post){
+
+return res.status(404).json({
+success:false,
+message:"Post not found"
+})
+
+}
+
+
+if(
+post.user.toString()
+!==req.user._id.toString()
+){
+
+return res.status(403).json({
+
+success:false,
+message:"Unauthorized"
+
+})
+
+}
+
+
+post.caption=
+req.body.caption
+|| post.caption;
+
+
+post.image=
+req.body.image
+|| post.image;
+
+
+await post.save();
+
+
+return res.status(200).json({
+
+success:true,
+message:"Post updated",
+post
+
+})
+
+}
+catch(error){
+
+return res.status(500).json({
+
+success:false,
+message:error.message
+
+})
+
+}
+
+};jsut
