fed252ef6aa1299ce323cba3f18c41dfeb20275a
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2026-05-24 21:54:09 +0530

    `Added deletePost function to postController.js`

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -111,4 +111,64 @@ message:error.message
 
 }
 
-};jsut
+};
+export const deletePost=async(req,res)=>{
+
+try{
+
+const post=
+await Post.findById(
+req.params.id
+);
+
+
+if(!post){
+
+return res.status(404).json({
+
+success:false,
+message:"Post not found"
+
+})
+
+}
+
+
+if(
+post.user.toString()
+!==req.user._id.toString()
+){
+
+return res.status(403).json({
+
+success:false,
+message:"Unauthorized"
+
+})
+
+}
+
+
+await post.deleteOne();
+
+
+return res.status(200).json({
+
+success:true,
+message:"Post deleted"
+
+})
+
+}
+catch(error){
+
+return res.status(500).json({
+
+success:false,
+message:error.message
+
+})
+
+}
+
+};
