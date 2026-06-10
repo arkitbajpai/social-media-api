8c416d50c8acd38c56df017075f44048cd97b904
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2026-06-10 22:29:44 +0530

    just a change

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -51,7 +51,7 @@ export const login = async(req,res)=>{
         {
             return res.status(404).json({
                 success:false,
-                message:"uemail and password is requried"
+                message:"uemail and password is requried sorry"
             })
         }
         
