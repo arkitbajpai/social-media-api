1d64dc9a9f2ddf98b5d80dac27fb1c503d7362af
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2026-07-16 21:45:29 +0530

    just a change

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -66,7 +66,7 @@ export const getCommentsByPost= async(req,res)=>{
 
 
      if(comment.user.toString()!== req.user._id.toString()){
-       return res.status(403).json({success:false,message:"Unauthorized"});
+       return res.status(403).json({success:false,message:"Unauthorized sorry!"});
      }
      comment.content =content || comment.content;
 
