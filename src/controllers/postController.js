58d09a8b209fa36a9c1aefcdeb144f6a4441cedf
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2026-07-20 22:24:15 +0530

    fixed the post controller

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,25 +1,30 @@
 import Post from "../models/Post.js";
 
-export const CreatePost=async(req,res)=>{
-    const {caption,image}=req.body;
-    
-    try{
-         const post = await Post.create({ caption, image, user:req.user._id})
-        
-         if(!post){
-              console.log("error in creating post");
-              return res.status(201).json({success:true,message:"Post created",post})
-            }
-          }
-        
-          
-          catch(error){
-             return res.status(404).json({message:error})
-           }
+export const CreatePost = async (req, res) => {
+    const { caption, image } = req.body;
 
+    try {
+        const post = await Post.create({
+            caption,
+            image,
+            user: req.user._id
+        });
 
-}
+        return res.status(201).json({
+            success: true,
+            message: "Post created successfully",
+            post
+        });
 
+    } catch (error) {
+        console.error("Error creating post:", error);
+
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
 
 export const getAllPosts = async(req,res)=>{
     
